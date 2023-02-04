f1217bd638e70cef91cbfe9cf77cfdc9bf99bc6f
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-04 17:16:41 +0530

    multiple urls are now working

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=> {
-    console.log("My name is Abhishek");
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    if(req.url == "/home"){
+        res.write('<body><h1>Welcome to the home page</h1></body>');
+    }
+    if(req.url == "/about"){
+        res.write('<body><h1>Welcome to the About page</h1></body>');
+
+    }
+    if(req.url == "/node"){
+        res.write('<body><h1>Welcome to My node project</h1></body>');
+
+    }
+    res.end();
+
 })
 
 server.listen(3000);
