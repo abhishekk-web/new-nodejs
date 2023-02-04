db5f46f83be0dafb06d7af6e43d57cb001235e7e
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-04 22:29:49 +0530

    reading and writing and storing the data in a file

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=> {
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    if(req.url == "/home"){
-        res.write('<body><h1>Welcome to the home page</h1></body>');
-    }
-    if(req.url == "/about"){
-        res.write('<body><h1>Welcome to the About page</h1></body>');
+    
+    const body = [];
+    
+    const url = req.url;
+    const method = req.method;
+
+    if(url==="/"){
+
+        // const filePath = path.join(__dirname, "messages.txt");
+
+        fs.readFile("message.txt", {encoding: "utf-8"}, (err, data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("the data is "+ data);
+        res.write("<html>");
+        res.write("<head><title>Welcome home</title></head>");
+        res.write(`<body><h1>${data}</h1></body>`)
+        res.write('<body><form action="/message" method="POST"><input id="message" type="text" name="message"><button>Send</button></form><body>');
+        res.write("</html>");
+
+        return res.end();
+        });
 
     }
-    if(req.url == "/node"){
-        res.write('<body><h1>Welcome to My node project</h1></body>');
+
+
+
+    else if(url==="/message" && method==='POST'){
+
+        req.on('data', (chunk)=>{
+        const newBody =    body.push(chunk);
+        console.log(newBody);
+
+        });
+
+
+        return req.on('end', ()=> {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile('message.txt', message, (err)=>{
+            if(err){
+
+                console.log(err);
+
+            }
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+        });
+    });
+
 
     }
+    else{
+    res.setHeader('Content-header', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>Welcome to About Us page</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
     res.end();
+    }  
 
 })
 
